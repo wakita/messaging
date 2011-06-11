952ac26f49c02243ab0b24fb49bae221e8353220
Ken Wakita <wakita@is.titech.ac.jp>
2011-06-11 14:54:59 +0900

    ひとまず，future 通信ができたらしい． Actor を使った階乗の計算が実行できた．

--- messaging.js
+++ messaging.js
@@ -1,5 +1,74 @@
+var debug = true;
 var messaging = {};
 
-messaging.start = function () {
+(function () {
+    var UNKNOWN = ' Unknown Message ', REPLY = ' Reply ';
+    var handler = {};
+    var on_replies = {};
+    var reply_id = 0;
 
-};
+    var delay = function () { return Math.floor(Math.random() * 1000); };
+
+    var dispatch = function (ev) {
+      var command = ev.command;
+      var ms = delay();
+      setTimeout(function () {
+//        alert('Dispatching [' + command + '] after ' + ms + 'ms delay');
+          (handler[command] || handler[UNKNOWN])(ev);
+        },
+        ms);
+    };
+
+    var reply = function (reply_to, v) {
+      setTimeout(function () {
+          var on_reply = on_replies[reply_to];
+          delete on_replies[reply_to];
+          on_reply(v);
+        },
+        delay());
+    };
+
+    var future = function (dispatch, ev, on_reply) {
+      if (on_reply) {
+        ev.reply_to = ' ' + reply_id + ' ';
+        on_replies[ev.reply_to] = on_reply;
+        reply_id++;
+      }
+      dispatch(ev);
+    };
+
+    var future_factory = function (/* dispatch */) {
+      return function (command, arg, on_reply) {
+        future(dispatch, { command: command, arg: arg }, on_reply);
+      }
+    };
+
+    var handle = function (command, do_handle) {
+      handler[command] = function (ev) {
+        var reply = function (v) {
+          if (ev.reply_to) {
+//          alert('Reply to: ' + ev.reply_to);
+            future(dispatch,
+              { command: REPLY, arg: { reply_to: ev.reply_to, value: v } });
+          }
+        }
+        do_handle(ev.arg, reply);
+      };
+    };
+
+    handle(UNKNOWN,
+      function (name) {
+        alert('Unknown message name (' + name + ')');
+      });
+
+    handle(REPLY,
+      function (info) { reply(info.reply_to, info.value); });
+
+    messaging.future_factory = future_factory;
+    messaging.handle = handle;
+
+    if (debug) {
+      messaging.handler = handler;
+      messaging.on_replies = on_replies;
+    }
+  })();
--- test.js
+++ test.js
@@ -1,3 +1,23 @@
-(function () {
-    alert('Messaging installed');
-  })();
+var future = messaging.future_factory();
+var install_message_handler = messaging.handle;
+
+install_message_handler('double',
+  function (n, reply) { reply(n * 2); });
+
+function run1() { future('double', 10, alert); }
+
+function run2() {
+
+  install_message_handler('fact',
+    function (n, reply) {
+      if (n == 0) reply(1);
+      else
+        future('fact', n - 1,
+          function (f) { future('multiply', { x: n, y: f }, reply); });
+    });
+
+  install_message_handler('multiply',
+    function (arg, reply) { reply(arg.x * arg.y); });
+
+  future('fact', 4, alert);
+}
